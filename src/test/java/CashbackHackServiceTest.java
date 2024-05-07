d6c283a9e7714b150ff7315bdd1affe503b56d34
Neto test <xmag91@mail.ru>
2024-05-07 21:30:21 +0300

    new branch

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,17 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldCountboundary(){
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1_200);
+        int expected = 800;
+        Assert.assertEquals(actual,expected);
+
+    }
+
+
+}
