f1feceea2b242c938d69304c8d937d3902f468a5
Neto test <xmag91@mail.ru>
2024-05-08 00:30:18 +0300

    new commit2

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -4,12 +4,12 @@ import ru.netology.service.CashbackHackService;
 
 public class CashbackHackServiceTest {
     @Test
-    public void shouldCountboundary(){
+    public void shouldCountboundary() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1_200);
         int expected = 800;
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
 
     }
 
