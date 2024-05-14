82b5340ad246fc8d6b59033a1e20765d4160ff0a
Neto test <xmag91@mail.ru>
2024-05-14 16:25:53 +0300

    Fixed2 commit

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -13,5 +13,15 @@ public class CashbackHackServiceTest {
 
     }
 
+    @Test
+    public void shouldCountboundary2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1_200);
+        int expected = 800;
+        Assert.assertEquals(expected, actual);
+
+    }
+
 
 }
